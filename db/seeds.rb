26abc3cf33cac5c2fc1314ff2d4ebf0d5005aa10
github username <github email address>
2019-06-10 23:43:41 -0400

    add seed file to database

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+User.create(name: "Steve", email: "stevem@gmail.com", password: "medabest")
+User.create(name: "David", email: "david@hotmail.com", password: "codingsawesome")
+Task.create(content: "I must move the car", user_id: 1 )
+Task.create(content: "need to take notes", user_id: 1 )
+Task.create(content: "go to work", user_id: 2 )
