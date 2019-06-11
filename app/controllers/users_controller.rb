6a6db755a787fc366513574d7f375932fa12570d
github username <github email address>
2019-06-11 13:52:34 -0400

    add logic to post login route, add structure for users show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
   get '/login' do
    erb :login
   end
+
+  post '/login' do
+    @user = User.find_by(email: params[:email])
+
+    if @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+
+       redirect to "users/#{@user.id}"
+    else
+
+    end
+  end
+   #users show route
+  get '/users/:id' do
+    "users show route"
+  end
 end
